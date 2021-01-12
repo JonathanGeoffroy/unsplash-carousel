365f2367b80a8e0161eb39cea7ef65278cc6a689
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2021-01-12 17:50:42 +0100

    Thumbnail autoscroll

--- packages/webapp/src/Thumbnails.js
+++ packages/webapp/src/Thumbnails.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useLayoutEffect, useRef } from "react";
 import { CarouselContext } from "./Carousel";
 import "./Thumbnails.scss";
 
@@ -8,10 +8,35 @@ export default function Thumbnails() {
     handleGoto,
   } = useContext(CarouselContext);
 
+  const selectedRef = useRef();
+
+  useLayoutEffect(() => {
+    if (selectedRef.current) {
+      const node = selectedRef.current;
+      const nodeLeft = node.offsetLeft;
+      const nodeRight = node.offsetLeft + node.clientWidth;
+
+      const parent = selectedRef.current.parentElement;
+      const parentLeft = parent.scrollLeft;
+      const parentRight = parent.scrollLeft + parent.clientWidth;
+
+      if (nodeLeft > parentRight || nodeRight < parentLeft) {
+        parent.scrollTo({
+          left: nodeLeft,
+          behavior: "smooth",
+        });
+      }
+    }
+  }, [selectedIndex]);
+
   return (
     <div className="thumbnails">
       {data.map((item, index) => (
-        <button key={item.id} onClick={() => handleGoto(index)}>
+        <button
+          key={item.id}
+          onClick={() => handleGoto(index)}
+          ref={index === selectedIndex ? selectedRef : undefined}
+        >
           <img
             className={index === selectedIndex ? "selected" : ""}
             src={item.thumbnailUrl}
